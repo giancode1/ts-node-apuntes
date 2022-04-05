9b2b4fb12ad43f6ac5a7602a8677e913863f1136
giancode1 <gieg5c@gmail.com>
2022-04-04 21:48:51 -0500

    Parametros rest

--- src/01-enum.ts
+++ src/01-enum.ts
@@ -1,10 +1,10 @@
-enum ROLES{
+export enum ROLES{
     ADMIN = 'admin',
     SELLER = 'user',
     CUSTOMER = 'customer'
 }
 
-type User = {
+export type User = {
   username: string;
   role: ROLES;
 }
