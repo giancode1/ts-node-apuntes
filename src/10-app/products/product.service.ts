dd1d3603faea945f212c69cb83288e203bb66d44
giancode1 <gieg5c@gmail.com>
2022-04-05 21:35:42 -0500

    Ejemplo de CRUD

--- src/10-app/products/product.service.ts
+++ src/10-app/products/product.service.ts
@@ -6,3 +6,19 @@ export const addProduct = (data: Product) => {
   //data.id = 'asdas',  //no se puede cambiar el id, es readonly
   products.push(data);
 }
+
+export const updateProduct = (id: string, changes: Product) => {
+  //code
+}
+
+export const deleteProduct = (id: string) => {
+  //code
+}
+
+export const getProduct = (id: string) => {
+  //code
+}
+
+export const searchProduct = (id: string) => {
+  //code
+}
